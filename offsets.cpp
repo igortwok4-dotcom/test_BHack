0189e617d76daf4cea1f504d5be274c309077a34
igortwok4-dotcom <igortwok4@gmail.com>
2026-01-19 16:27:15 +0300

    Update offsets.cpp

--- offsets.cpp
+++ offsets.cpp
@@ -1,39 +1,18 @@
-// offsets.cpp
 #include "offsets.h"
 #include "memory.h"
 
-uintptr_t Offsets::dwLocalPlayerPawn = 0;
-uintptr_t Offsets::dwEntityList = 0;
-uintptr_t Offsets::dwViewMatrix = 0;
-uintptr_t Offsets::m_iHealth = 0x334;
-uintptr_t Offsets::m_iTeamNum = 0x3C3;
-uintptr_t Offsets::m_vecOrigin = 0x12CC;
-uintptr_t Offsets::m_aimPunchAngle = 0x1784;
-uintptr_t Offsets::m_pGameSceneNode = 0x320;
-uintptr_t Offsets::m_modelState = 0x170;
-uintptr_t Offsets::m_boneArray = 0x80;
-uintptr_t Offsets::v_angle = 0x23B0;
-
-std::string Offsets::sigLocalPlayerPawn = "\x48\x8B\x05\x00\x00\x00\x00\x48\x85\xC0\x74\x06\x48\x39\x88";
-std::string Offsets::maskLocalPlayerPawn = "xxx????xxxxxxxx";
-std::string Offsets::sigEntityList = "\x48\x8B\x0D\x00\x00\x00\x00\x48\x89\x7C\x24\x00\x8B\xFA\xC1";
-std::string Offsets::maskEntityList = "xxx????xxxx?xxx";
-std::string Offsets::sigViewMatrix = "\x48\x8D\x0D\x00\x00\x00\x00\x48\xC7\x44\x24\x00\x00\x00\x00\x00\x48\x8B\x10";
-std::string Offsets::maskViewMatrix = "xxx????xxxx?xxxxxxxxx";
-
 void Offsets::Init(uintptr_t moduleBase, HANDLE hProcess) {
-    uintptr_t addr = Memory::FindSignature(hProcess, moduleBase, 0x8000000, sigLocalPlayerPawn, maskLocalPlayerPawn);
-    if (addr) {
-        dwLocalPlayerPawn = Memory::Read<int32_t>(hProcess, addr + 3) + addr + 7 - moduleBase;
-    }
+    dwLocalPlayerPawn   = 0x1B0A3A0;
+    dwEntityList        = 0x1E0E7D0;
+    dwViewMatrix        = 0x1EA3B60;
 
-    addr = Memory::FindSignature(hProcess, moduleBase, 0x8000000, sigEntityList, maskEntityList);
-    if (addr) {
-        dwEntityList = Memory::Read<int32_t>(hProcess, addr + 3) + addr + 7 - moduleBase;
-    }
-
-    addr = Memory::FindSignature(hProcess, moduleBase, 0x8000000, sigViewMatrix, maskViewMatrix);
-    if (addr) {
-        dwViewMatrix = Memory::Read<int32_t>(hProcess, addr + 3) + addr + 7 - moduleBase;
-    }
+    m_iHealth           = 0x334;
+    m_iTeamNum          = 0x3C3;
+    m_vecOrigin         = 0x12CC;
+    m_aimPunchAngle     = 0x1784;
+    m_pGameSceneNode    = 0x320;
+    m_modelState        = 0x170;
+    m_boneArray         = 0x80;
+    m_hObserverTarget   = 0x34B0;
+    v_angle             = 0x23B0;
 }
