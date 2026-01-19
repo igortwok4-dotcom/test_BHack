49c270357af2e8483d8cfec7c0fdcd610daa14d3
igortwok4-dotcom <igortwok4@gmail.com>
2026-01-19 16:15:40 +0300

    Update aimbot.h

--- aimbot.h
+++ aimbot.h
@@ -1,9 +1,28 @@
-// aimbot.h
 #ifndef AIMBOT_H
 #define AIMBOT_H
 
 #include <windows.h>
+#include <cstdint>  // для uintptr_t
 
+struct Vector3 {
+    float x, y, z;
+
+    Vector3 operator-(const Vector3& other) const {
+        return { x - other.x, y - other.y, z - other.z };
+    }
+};
+
+namespace Config {
+    extern bool aimbotEnabled;
+    extern float aimbotFov;
+    extern float aimbotSmooth;
+    extern int aimbotBone;
+}
+
+// Основные функции aimbot'а
+Vector3 CalcAngle(const Vector3& src, const Vector3& dst);
+Vector3 GetBonePos(HANDLE hProcess, uintptr_t player, int bone);
+bool IsVisible(HANDLE hProcess, uintptr_t player);
 void UpdateAimbot(HANDLE hProcess, uintptr_t moduleBase, uintptr_t localPlayer);
 
-#endif
+#endif // AIMBOT_H
