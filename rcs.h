b2fd9932b08126336e8868fbb4d20c26f762846c
igortwok4-dotcom <igortwok4@gmail.com>
2026-01-19 17:09:40 +0300

    Update rcs.h

--- rcs.h
+++ rcs.h
@@ -3,14 +3,8 @@
 
 #include <windows.h>
 #include <cstdint>
+#include <memory.h>
 
-struct Vector3 {
-    float x, y, z;
-
-    Vector3 operator-(const Vector3& other) const {
-        return { x - other.x, y - other.y, z - other.z };
-    }
-};
 
 namespace Config {
     extern bool rcsEnabled;
