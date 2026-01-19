b600fcb612fb4fb9a90551273af1c97295a39a88
igortwok4-dotcom <igortwok4@gmail.com>
2026-01-19 15:43:09 +0300

    Update offsets.h

--- offsets.h
+++ offsets.h
@@ -4,29 +4,29 @@
 
 #include <windows.h>
 
-// Structures
 struct Vector3 {
     float x, y, z;
 };
 
-struct Player {
-    Vector3 position;
-    int health;
-    int team;
-    Vector3 viewAngles;
-    // Add more as needed
-};
-
-// Offsets (hardcoded for simplicity, use sigscan in Init)
 namespace Offsets {
-    extern uintptr_t dwLocalPlayer;
+    extern uintptr_t dwLocalPlayerPawn;
     extern uintptr_t dwEntityList;
+    extern uintptr_t dwViewMatrix;
     extern uintptr_t m_iHealth;
     extern uintptr_t m_iTeamNum;
     extern uintptr_t m_vecOrigin;
     extern uintptr_t m_aimPunchAngle;
-    extern uintptr_t m_hBoneMatrix;
-    // etc.
+    extern uintptr_t m_pGameSceneNode;
+    extern uintptr_t m_modelState;
+    extern uintptr_t m_boneArray;
+    extern uintptr_t v_angle;
+
+    extern std::string sigLocalPlayerPawn;
+    extern std::string maskLocalPlayerPawn;
+    extern std::string sigEntityList;
+    extern std::string maskEntityList;
+    extern std::string sigViewMatrix;
+    extern std::string maskViewMatrix;
 
     void Init(uintptr_t moduleBase, HANDLE hProcess);
 }
