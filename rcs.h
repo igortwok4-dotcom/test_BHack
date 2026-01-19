a7d91523de16c7960a17d5f590acef48fa4f6d17
igortwok4-dotcom <igortwok4@gmail.com>
2026-01-19 16:30:23 +0300

    Update rcs.h

--- rcs.h
+++ rcs.h
@@ -1,9 +1,22 @@
-// rcs.h
 #ifndef RCS_H
 #define RCS_H
 
 #include <windows.h>
+#include <cstdint>
+
+struct Vector3 {
+    float x, y, z;
+
+    Vector3 operator-(const Vector3& other) const {
+        return { x - other.x, y - other.y, z - other.z };
+    }
+};
+
+namespace Config {
+    extern bool rcsEnabled;
+    extern float rcsStrength;
+}
 
 void UpdateRCS(HANDLE hProcess, uintptr_t moduleBase, uintptr_t localPlayer);
 
-#endif
+#endif // RCS_H
